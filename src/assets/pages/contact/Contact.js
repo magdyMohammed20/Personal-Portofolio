57afdaa6ed2a79a45275a97f76a4e571157673bc
magdyMohammed20 <magdymohammed37@yahoo.com>
2021-08-26 17:00:11 +0200

    Create Contact Page

--- src/assets/pages/contact/Contact.js
+++ src/assets/pages/contact/Contact.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {ContactSection , Span} from './Styles'
+import ContactTemp from './ContactTemp'
 
 function Contact() {
     return (
-        <div>
-            Contact
-        </div>
+        <ContactSection className='p-8 h-auto lg:h-screen'>
+            <div className='container mx-auto px-6'>
+                <div className='text-center mt-8'>
+                    <Span>CONTACT</Span>
+                    <h2 className='text-3xl text-white font-bold'>GET IN TOUCH</h2>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 text-center">
+                    <ContactTemp icon='fa fa-phone fa-2x' name1='phone' desc='01012935745'/>
+                    
+                    <ContactTemp icon='fa fa-map-marker fa-2x' name1='address' desc='cairo'/>
+                    <ContactTemp icon='fa fa-envelope fa-2x' name1='email' desc='magdymohammed96@yahoo.com'/>
+                </div>
+            </div>
+        </ContactSection>
     )
 }
 
 export default Contact
+
---
+++ src/assets/pages/contact/Styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const mainColor = '#111827';
+const mainColor2 = '#050b19';
+const secondColor = '#FB839E';
+
+
+export const ContactSection = styled.div`
+    background-color: ${mainColor}
+`;
+
+export const Span = styled.span`
+    color: ${secondColor}
+`;
+
+export const Div = styled.div`
+`;
+
+export const Icon = styled.i`
+color: ${secondColor}
+`;
