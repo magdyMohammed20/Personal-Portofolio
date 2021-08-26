56e72b315c70bf548b471321899e6bdb172f92b3
magdyMohammed20 <magdymohammed37@yahoo.com>
2021-08-26 13:34:16 +0200

    Handle Skill Width Default Value

--- src/assets/pages/skills/SkillTemp.js
+++ src/assets/pages/skills/SkillTemp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Div} from './Styles'
 
-function SkillTemp({src , skillName , opClass , opWidth}) {
+function SkillTemp({src , skillName , opClass , opWidth=''}) {
     return (
         <div className={`mt-4 text-center p-4 pb-0 ${opWidth}`}>
             <Div className='bg-gray-800 p-5 cursor-pointer rounded-lg'>
