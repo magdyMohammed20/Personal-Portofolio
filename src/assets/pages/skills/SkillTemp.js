86e8f515c7e07d851f236f76ce799ee56eac7814
magdyMohammed20 <magdymohammed37@yahoo.com>
2021-08-27 14:31:37 +0200

    Fix Skill Size

--- src/assets/pages/skills/SkillTemp.js
+++ src/assets/pages/skills/SkillTemp.js
@@ -4,8 +4,8 @@ import {Div} from './Styles'
 function SkillTemp({src , skillName , opClass , opWidth=''}) {
     return (
         <div className={`mt-4 text-center p-4 pb-0 ${opWidth}`}>
-            <Div className='bg-gray-800 p-5 cursor-pointer rounded-lg'>
-                <img src={src} className={`h-12 mx-auto ${opClass}`}/>
+            <Div className='bg-gray-800 px-3 py-4 cursor-pointer rounded-lg'>
+                <img src={src} className={`h-12 w-12 mx-auto ${opClass}`}/>
                 <span className='text-gray-400 font-bold mt-2 block'>{skillName}</span>
             </Div>
         </div>
