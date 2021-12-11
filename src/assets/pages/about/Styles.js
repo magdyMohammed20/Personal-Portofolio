7581821852829a6da60b6dea52a9d436ee2c1c58
magdyMohammed20 <magdymohammed37@yahoo.com>
2021-12-11 14:37:40 +0200

    Change About Color Theme

--- src/assets/pages/about/Styles.js
+++ src/assets/pages/about/Styles.js
@@ -1,50 +1,38 @@
 import styled from 'styled-components'
+import Colors from '../../aids/Colors'
 
-const mainColor = '#111827';
-const mainColor2 = '#050b19';
-const secondColor = '#FB839E';
-
+const { mainColor, secondColor, btnColor1, whiteColor } = Colors
 
 export const AboutSection = styled.div`
     height: 100vh;
-    background-color: ${mainColor}
+    background-color: ${secondColor}
 `;
 
 export const Span = styled.span`
-    color: #FB839E
+    color: ${mainColor}
 `;
-
-/*export const Img = styled.img`
-width: 400px;
-    height: 400px;
-    -webkit-filter: invert(16%) sepia(80%);
-    filter: invert(16%) sepia(80%);
-`;
-*/
 
 export const Anchor = styled.a`
     transition: all .3s linear;
 
    &:hover{
-       background-color: ${secondColor};
+       background-color: ${mainColor};
    }
 
    i{
     transition: all .3s linear;
    }
    &:hover > i{
-       color: ${mainColor} !important;
        transform: rotate(360deg);
    }
 `;
 
 export const Btn = styled.a`
-    background-color: ${mainColor2};
-    color: ${secondColor};
+    background-color: ${btnColor1};
+    color: ${whiteColor};
     transition: all .4s linear;
 
     &:hover{    
-        background-color: #cdb78e;
-        color: #2b1266
+        background-color: ${mainColor};
     }
 `;
