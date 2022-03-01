a643cd6b13c3d8508d82e4460437165d87cbc108
magdyMohammed20 <magdymohammed37@yahoo.com>
2022-03-01 02:02:19 +0200

    Add Countries React API App

--- src/assets/pages/portofolio/Portofolio.js
+++ src/assets/pages/portofolio/Portofolio.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {PortofolioSection} from './Styles'
+import { PortofolioSection } from './Styles'
 import PortoTemp from './PortoTemp'
 import Bright from '../../images/Portofolio/Bright.PNG'
 import Education from '../../images/Portofolio/Education.PNG'
 import NewEvent from '../../images/Portofolio/NewEvent.PNG'
 import Tailwind from '../../images/Portofolio/Tailwind.PNG'
 import TheEvent from '../../images/Portofolio/TheEvent.PNG'
-
+import Countries from '../../images/Portofolio/countries.PNG'
 
 function Portofolio() {
     return (
@@ -16,11 +16,12 @@ function Portofolio() {
                     <h2 className='text-3xl text-white font-bold'>LATEST WORKS</h2>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 mt-6">
-                        <PortoTemp src={Bright} desc='Bright Template' link='https://bright-nine.vercel.app/'/>
-                        <PortoTemp src={Education} desc='Educational Template' link='https://educational-template.vercel.app/'/>
-                        <PortoTemp src={NewEvent} desc='Event Template' link='https://events-template.vercel.app/'/>
-                        <PortoTemp src={Tailwind} desc='Tailwind Template' link='https://tailwind-react-template.vercel.app/'/>
-                        <PortoTemp src={TheEvent} desc='Conference Template' link='https://application-on-bootstrap-4.vercel.app/'/>
+                    <PortoTemp src={Bright} desc='Bright Template' link='https://bright-nine.vercel.app/' />
+                    <PortoTemp src={Education} desc='Educational Template' link='https://educational-template.vercel.app/' />
+                    <PortoTemp src={NewEvent} desc='Event Template' link='https://events-template.vercel.app/' />
+                    <PortoTemp src={Tailwind} desc='Tailwind Template' link='https://tailwind-react-template.vercel.app/' />
+                    <PortoTemp src={TheEvent} desc='Conference Template' link='https://application-on-bootstrap-4.vercel.app/' />
+                    <PortoTemp src={Countries} desc='React Countries API' link='https://621d5b2171ee2e52b98616d9--competent-lichterman-f484dd.netlify.app/' />
                 </div>
             </div>
         </PortofolioSection>
